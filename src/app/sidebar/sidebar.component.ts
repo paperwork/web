06230af6598df1f2b63b8642d3639879a0555c8b
Marius <marius@twostairs.com>
2019-04-29 00:06:35 +0200

    Enhanced sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +9,38 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  tabs = [
+    {
+      id: 'notifications',
+      label: 'Notifications',
+    },
+    {
+      id: 'navigation',
+      label: 'Navigation',
+    },
+    {
+      id: 'folders',
+      label: 'Folders',
+    },
+    {
+      id: 'tags',
+      label: 'Tags',
+    },
+  ];
   selected = new FormControl(1);
 
-  constructor() { }
+  constructor(private router: Router, private navigation: NavigationService) { }
 
   ngOnInit() {
+    this.navigation.current.subscribe(id => {
+      this.tabs.forEach((tab, idx) => {
+        if(tab.id === id) {
+          this.selected.setValue(idx);
+        }
+      });
+    });
+    this.router.events.subscribe((res) => {
+      this.selected.setValue(this.tabs.indexOf(this.tabs.find(tab => tab.id === this.router.url.replace('/', ''))));
+    });
   }
-
 }
