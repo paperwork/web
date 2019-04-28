93365bc4243e271e3dd485a84b819afce1837331
Marius <marius@twostairs.com>
2019-04-29 00:06:58 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  AppComponent},
+  { path: 'notifications', component:  AppComponent},
+  { path: 'folders', component: AppComponent},
+  { path: 'tags', component: AppComponent},
+];
+
+export const appRouting = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
