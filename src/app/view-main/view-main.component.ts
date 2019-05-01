8a6cfcacb20df5b6785d92dafca29f0cf36b24f1
Marius <marius@twostairs.com>
2019-05-01 11:53:55 +0200

    Added responsiveness, added more classes for theming

--- src/app/view-main/view-main.component.ts
+++ src/app/view-main/view-main.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-view-main',
   templateUrl: './view-main.component.html',
   styleUrls: ['./view-main.component.scss']
 })
-export class ViewMainComponent implements OnInit {
+export class ViewMainComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList;
   opened: boolean = true; // TODO: Load from user prefs
   events: string[] = [];
 
-  constructor() { }
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
