b8169752c6e90838dd33c5715dccae98c38d58fa
Marius <marius@twostairs.com>
2019-05-05 18:01:23 +0200

    Implemented first shot for an editor

--- src/app/notes/partial-notes-show/partial-notes-show.component.ts
+++ src/app/notes/partial-notes-show/partial-notes-show.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { ToolbarService } from '../../partial-toolbar-main/toolbar.service';
 import { NotesService } from '../notes.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Note } from '../note';
 import { get } from 'lodash';
 
@@ -13,30 +14,50 @@ import { get } from 'lodash';
   styleUrls: ['./partial-notes-show.component.scss']
 })
 export class PartialNotesShowComponent implements OnInit {
+  noteId: string;
   note$: Observable<Note>;
   toolbarState: number;
+  modeEdit: boolean;
+  editor: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private notesService: NotesService,
-    private toolbarService: ToolbarService
+    private toolbarService: ToolbarService,
+    private formBuilder: FormBuilder
   ) {
     this.toolbarService.state$.subscribe((state: number) => {
       this.setAll(state);
     });
+
+    this.editor = formBuilder.group({
+      color: 'primary',
+      body: ['', Validators.min(10)],
+    });
   }
 
   ngOnInit() {
-    this.toolbarService.state = get(history, 'state.toolbarState', this.toolbarService.TOOLBAR_STATE_BACK_DEFAULT);
+    this.setAll(get(history, 'state.toolbarState', this.toolbarService.TOOLBAR_STATE_BACK_DEFAULT));
 
     this.note$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.notesService.show(params.get('id')))
+      switchMap((params: ParamMap) => {
+        this.noteId = params.get('id');
+        return this.notesService.show(this.noteId);
+      }),
+      tap(note => this.editor.patchValue(note))
     );
+
+    this.editor.get('body').valueChanges.subscribe(value => this.notesService.updateField(this.noteId, 'body', value));
   }
 
   setAll(state: number) {
     this.toolbarState = state;
+    if(state === this.toolbarService.TOOLBAR_STATE_BACK_MODE_EDIT) {
+      this.modeEdit = true;
+    } else {
+      this.modeEdit = false;
+      console.log(this.editor)
+    }
   }
 }
