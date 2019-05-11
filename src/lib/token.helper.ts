83e0fb497c93c4ab2d2e77e180a5d349bc9f2ba1
Marius <marius@twostairs.com>
2019-05-12 00:41:03 +0200

    Implemented token.helper

---
+++ src/lib/token.helper.ts
@@ -0,0 +1,71 @@
+interface IJsonWebTokenPayload {
+  jti: string;
+  typ: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  iss: string;
+}
+
+class JsonWebTokenPayload implements IJsonWebTokenPayload {
+  jti: string;
+  typ: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  iss: string;
+}
+
+interface IToken {
+  id: string;
+  type: string;
+  userId: string;
+  systemId: string;
+  userGid: string;
+  issueTime: Date;
+  expiryTime: Date;
+  issuer: string;
+}
+
+class Token implements IToken {
+  id: string;
+  type: string;
+  userId: string;
+  systemId: string;
+  userGid: string;
+  issueTime: Date;
+  expiryTime: Date;
+  issuer: string;
+
+  constructor(decodedTokenObject: JsonWebTokenPayload) {
+    this.id = decodedTokenObject.jti;
+    this.type = decodedTokenObject.typ;
+    this.userId = decodedTokenObject.sub;
+    this.systemId = decodedTokenObject.aud;
+    this.userGid = this.userId + '@' + this.systemId;
+    this.issueTime = new Date(decodedTokenObject.iat);
+    this.expiryTime = new Date(decodedTokenObject.exp);
+    this.issuer = decodedTokenObject.iss;
+  }
+}
+
+export const tokenGetDecoded = (access_token?: string|null): Token|null => {
+  if(typeof access_token !== 'string') {
+    access_token = localStorage.getItem('access_token');
+  }
+
+  if(access_token === null) {
+    return null;
+  }
+
+  const [header64, payload64, signature64] = access_token.split('.');
+
+  if(typeof payload64 !== 'string') {
+    return null;
+  }
+
+  const jwtPayload: JsonWebTokenPayload = JSON.parse(decodeURIComponent(escape(window.atob(payload64))));
+  return new Token(jwtPayload);
+};
