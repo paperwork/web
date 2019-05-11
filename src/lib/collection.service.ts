d7026a9937f4fc948c2f0138528ca07f296c4760
Marius <marius@twostairs.com>
2019-05-12 00:41:33 +0200

    Extended collection.service

--- src/lib/collection.service.ts
+++ src/lib/collection.service.ts
@@ -1,13 +1,18 @@
 import { Injectable, InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { List } from 'immutable';
 
 import Dexie from 'dexie';
 
 export interface ICollectionService {
   db: Dexie;
+  collection: Function;
+
   collectionName: string;
   index: string;
-  collection: Function;
+
+  readonly entries: Observable<List<any>>;
+
   onCollectionInit?(): Promise<boolean>;
   onCollectionChange?(changeset: Object): Promise<boolean>;
 }
@@ -22,6 +27,8 @@ export class CollectionService implements ICollectionService {
   public collectionName: string = '';
   public index: string = '';
 
+  public readonly entries: Observable<List<any>>;
+
   constructor() {
   }
 
@@ -35,5 +42,57 @@ export class CollectionService implements ICollectionService {
 
   get collection() {
     return this._db[this.collectionName];
+  }
+
+  getEntryIndexById<T extends { id: string; }>(entriesSubject: BehaviorSubject<List<T>>, entryId: string): number {
+    const entries: List<T> = entriesSubject.getValue();
+    let entryIndex: number = 0;
+
+    return entries.findIndex((entry: T) => entry.id === entryId);
+  }
+
+  getEntryByIndex<T>(entriesSubject: BehaviorSubject<List<T>>, entryIndex: number): T|null {
+    let entry: T|null = null;
+
+    if(entryIndex < 0) {
+      return null;
+    }
+
+    const entries: List<T> = entriesSubject.getValue();
+    return entries.get(entryIndex, null);
+  }
+
+  getEntryById<T extends { id: string; }>(entriesSubject: BehaviorSubject<List<T>>, entryId: string): T|null {
+    const entryIndex: number = this.getEntryIndexById(entriesSubject, entryId);
+    return this.getEntryByIndex(entriesSubject, entryIndex);
+  }
+
+  changeEntry<T extends { id: string; }>(entriesSubject: BehaviorSubject<List<T>>, action: string, entryId: string|null, newValue?: T): boolean {
+    const entries: List<T> = entriesSubject.getValue();
+    let entryIndex: number = -1;
+
+    if((action === 'updated' || action === 'deleted')
+    && typeof entryId === 'string') {
+      entryIndex = this.getEntryIndexById(entriesSubject, entryId);
+
+      if(entryIndex === -1) {
+        console.log('Could not find entry in entries!');
+        return false;
+      }
+    }
+
+    let newEntries: List<T>;
+    if(action === 'created' && typeof newValue !== 'undefined') {
+      newEntries = entries.push(newValue);
+    } else if(action === 'updated' && typeof newValue !== 'undefined') {
+      newEntries = entries.set(entryIndex, newValue);
+    } else if(action === 'deleted') {
+      newEntries = entries.delete(entryIndex);
+    } else {
+      return false;
+    }
+
+    entriesSubject.next(newEntries);
+    return true;
   }
 }
