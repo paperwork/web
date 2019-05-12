d7bd74bf5ccbda73fd5ff71b2c255fe34a5a80db
Marius <marius@twostairs.com>
2019-05-13 00:30:44 +0200

    Implemented further subjects, implemented ToolbarAction

--- src/app/partial-toolbar-main/toolbar.service.ts
+++ src/app/partial-toolbar-main/toolbar.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+export type ToolbarActionPayload = {
+  [key: string]: any;
+}
+
+export class ToolbarAction {
+  action: string;
+  payload: ToolbarActionPayload;
+
+  constructor(action: string, payload: ToolbarActionPayload) {
+    this.action = action;
+    this.payload = payload;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,14 +27,33 @@ export class ToolbarService {
   public TOOLBAR_STATE_CHECKBOX_SOME_SELECTED: number = 23;
   public TOOLBAR_STATE_CHECKBOX_ALL_SELECTED: number = 24;
 
-  private readonly _state = new BehaviorSubject<number>(this.TOOLBAR_STATE_BACK_DEFAULT);
-  readonly state$ = this._state.asObservable();
-
   constructor(
   ) {
   }
 
+  private readonly _state = new BehaviorSubject<number>(this.TOOLBAR_STATE_BACK_DEFAULT);
+
+  public readonly state$ = this._state.asObservable();
+
   public set state(val: number) {
     this._state.next(val);
   }
+
+
+  private readonly _search = new BehaviorSubject<string>('');
+
+  public readonly search$ = this._search.asObservable();
+
+  public set search(val: string) {
+    this._search.next(val);
+  }
+
+  private readonly _actions = new BehaviorSubject<ToolbarAction>(new ToolbarAction('', {}));
+
+  public readonly actions$ = this._actions.asObservable();
+
+  public set trigger(toolbarAction: ToolbarAction) {
+    this._actions.next(toolbarAction);
+  }
+
 }
