e6358dd0e5ca4689bb96a7beee8319d75ff883ae
Marius <marius@twostairs.com>
2019-05-13 00:34:35 +0200

    Implemented real tags

--- src/app/partial-sidebar-tags/partial-sidebar-tags.component.ts
+++ src/app/partial-sidebar-tags/partial-sidebar-tags.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Note } from '../notes/note';
-import { MockNotes } from '../notes/mock';
-import { map, reduce, merge } from 'lodash';
+import { NotesService } from '../notes/notes.service';
+import { Subscription } from 'rxjs';
+import { union } from 'lodash';
+import { List } from 'immutable';
 
 @Component({
   selector: 'partial-sidebar-tags',
@@ -9,17 +11,28 @@ import { map, reduce, merge } from 'lodash';
   styleUrls: ['./partial-sidebar-tags.component.scss']
 })
 export class PartialSidebarTagsComponent implements OnInit, OnDestroy {
+  private notesServiceSubscription: Subscription;
   tags: Array<string> = [];
 
-  constructor() {
+  constructor(
+    private notesService: NotesService
+  ) {
   }
 
   ngOnInit() {
-    this.tags = reduce(map(MockNotes, (note: Note) => {
-      return note.tags;
-    }), (res: Array<string>, tags: Array<string>) => {
-      return merge(res, tags);
-    }, []);
+    this.notesServiceSubscription = this.notesService.entries.subscribe((notes: List<Note>) => {
+      this.tags = notes.map((note: Note) => {
+        return note.get('tags');
+      }).reduce((res: Array<string>, tags: Array<string>) => {
+        return union(res, tags);
+      }, []);
+    })
+  }
+
+  ngOnDestroy() {
+    if(typeof this.notesServiceSubscription !== 'undefined') {
+      this.notesServiceSubscription.unsubscribe();
+    }
   }
 
 }
