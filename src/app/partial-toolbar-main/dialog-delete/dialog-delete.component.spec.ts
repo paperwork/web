63591734da5f98f202c954df4c05b1721322e2f9
Marius <marius@twostairs.com>
2019-05-18 13:36:51 +0200

    Implemented DialogDeleteComponent

---
+++ src/app/partial-toolbar-main/dialog-delete/dialog-delete.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogDeleteComponent } from './dialog-delete.component';
+
+describe('DialogDeleteComponent', () => {
+  let component: DialogDeleteComponent;
+  let fixture: ComponentFixture<DialogDeleteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DialogDeleteComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DialogDeleteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/partial-toolbar-main/dialog-delete/dialog-delete.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'dialog-duplicate',
+  templateUrl: './dialog-delete.component.html',
+  styleUrls: ['./dialog-delete.component.scss']
+})
+export class DialogDeleteComponent implements OnInit, OnDestroy {
+  constructor(
+    public dialogRef: MatDialogRef<DialogDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) {
+  }
+
+  ngOnInit() {
+    this.data.sure = true;
+  }
+
+  ngOnDestroy() {
+  }
+
+  onCancel(): void {
+    this.data.sure = false;
+    this.dialogRef.close();
+  }
+
+}
