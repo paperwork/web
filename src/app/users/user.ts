95ef71317fa355b68657f688b7f7819db9fbaa0a
Marius <marius@twostairs.com>
2019-05-26 17:13:18 +0200

    Extended User

--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -3,12 +3,28 @@ import { merge } from 'lodash';
 
 export interface IUser {
   id: string;
+  gid: string;
+  email: string;
+  name: {
+    first_name: string;
+    last_name: string;
+  };
+  profile_photo: string;
+  role: string;
   created_at?: Date;
   deleted_at?: Date;
 }
 
 const UserRecord = Record({
   id: '',
+  gid: '',
+  email: '',
+  name: {
+    first_name: '',
+    last_name: '',
+  },
+  profile_photo: '',
+  role: '',
   created_at: new Date(),
   deleted_at: null
 });
