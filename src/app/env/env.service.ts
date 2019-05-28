b2553be952b62ccfe8059e072372b30bb350efb3
Marius <marius@twostairs.com>
2019-05-28 22:51:24 +0200

    Refactored envService, added status

--- src/app/env/env.service.ts
+++ src/app/env/env.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { from } from 'rxjs';
 import { filter, map, switchMap, tap } from 'rxjs/operators';
 declare var window: any;
+
+export type TEnvStatus = {
+  initialized: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvService {
+  private _status: BehaviorSubject<TEnvStatus> = new BehaviorSubject({ 'initialized': false });
+  public readonly status: Observable<TEnvStatus> = this._status.asObservable();
 
   constructor() {
     this.init();
   }
 
-  public init() {
-    return from(fetch('/.env.json')
+  public async init() {
+    await from(fetch('/.env.json')
       .then(function(response) {
         return response.json();
       }))
@@ -22,6 +29,8 @@ export class EnvService {
       return
     }))
     .toPromise();
+
+    this._status.next({ 'initialized': true });
   }
 
   public get(key: string): string {
