8e378379a2fd51fbc878e506d1a89ab14b0a6e32
Marius <marius@twostairs.com>
2019-06-09 11:34:02 +0200

    Extended sync.service, built first mock sync between Angular and PouchDB

--- src/app/api/sync.service.ts
+++ src/app/api/sync.service.ts
@@ -1,24 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subject, BehaviorSubject, Subscription } from 'rxjs';
 import { first, tap } from 'rxjs/operators';
-import { forEach, omit } from 'lodash';
+import { forEach, omit, get, map } from 'lodash';
 import { List, Record } from 'immutable';
 
 import { ICollectionService } from '../../lib/collection.service';
 import { EnvService, TEnvStatus } from '../env/env.service';
 import { AlertService } from '../partial-alert/alert.service';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/user';
+import { UsersJournalsService } from '../users/journals/journals.service';
+import { Journal } from '../users/journals/journal';
 import { NotesService } from '../notes/notes.service';
+import { Note } from '../notes/note';
 
-type TEntry = {
+import { getSyncId, setSyncId } from '../../lib/sync.helper';
+
+export type TEntry = {
   id: string;
   [key: string]: any;
 }
 
+export type TPouchResponseRow = {
+  doc: {
+    _id: string;
+    _rev: string;
+    _attachments: Object;
+    [key: string]: any;
+  };
+}
+
+export type TPouchResponse = {
+  offset: number;
+  total_rows: number;
+  rows: Array<TPouchResponseRow>;
+};
+
 @Injectable({
   providedIn: 'root'
 })
-export class SyncService {
+export class SyncService implements OnInit, OnDestroy {
   envStatus: Subscription = null;
   collectionSubscriptions: Array<Subscription> = [];
 
@@ -26,62 +47,173 @@ export class SyncService {
     private envService: EnvService,
     private alertService: AlertService,
     private usersService: UsersService,
+    private usersJournalsService: UsersJournalsService,
     private notesService: NotesService
   ) {
-    this.envStatus = envService.status.subscribe((status: TEnvStatus) => {
-      console.debug('Retrieved EnvService status:', status);
-      if(typeof status === 'object' && status.initialized === true && status.loggedIn === true) {
-        console.debug('apiLoad(usersService)');
-        this.apiLoad(usersService);
+    console.debug('Constructing SyncService ...');
+    this.ngOnInit();
+  }
 
-        console.debug('apiLoad(notesService)');
-        this.apiLoad(notesService);
+  ngOnInit() {
+    console.debug('Initializing SyncService ...');
+    this.envStatus = this.envService.status.subscribe((status: TEnvStatus) => {
+      console.debug('Retrieved EnvService status:', status);
+      if(this.canSync(status) === true) {
+        // console.debug('apiLoad(usersService)');
+        // this.apiLoad(this.usersService, User);
+
+        // console.debug('apiLoad(usersJournalsService)');
+        // this.apiLoad(this.usersJournalsService, Journal);
+
+        // console.debug('apiLoad(notesService)');
+        // this.apiLoad(this.notesService, Note);
+
+        this.sync();
       }
     });
 
-    this.subscribeToCollectionService(usersService);
-    this.subscribeToCollectionService(notesService);
+    this.subscribeToCollectionService(this.usersService, User);
+    this.subscribeToCollectionService(this.usersJournalsService, Journal);
+    this.subscribeToCollectionService(this.notesService, Note);
   }
 
-  private subscribeToCollectionService(collectionService: ICollectionService): boolean {
+  ngOnDestroy() {
+    this.unsubscribeCollectionSubscriptions();
+  }
+
+  canSync(status: TEnvStatus): boolean {
+    if(typeof status === 'object'
+    && status.initialized === true
+    && status.loggedIn === true
+    && status.domReady === true) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public async sync(): Promise<boolean> {
+    const status: TEnvStatus = this.envService.getStatus();
+    if(this.canSync(status) === false) {
+      return false;
+    }
+
+    console.debug('Syncing UsersJournalsService ...');
+    await this.apiLoad(this.usersJournalsService, Journal, {}, null, null, async (collectionService: ICollectionService, newJournalEntries: List<Journal>) => {
+      return this.syncJournalReflector(collectionService, newJournalEntries);
+    });
+
+    return true;
+  }
+
+  private async syncJournalReflector(collectionService: ICollectionService, newJournalEntries: List<Journal>) {
+      console.debug('Checking currently available journal entries:', newJournalEntries);
+
+      if(newJournalEntries.size > 0) {
+        console.debug('Got new journal entries! Checking ...');
+
+        const newJournalNoteEntries: List<Journal> = newJournalEntries.filter((journalEntry: Journal) => journalEntry.resource === 'note');
+        if(newJournalNoteEntries.size > 0) {
+          console.debug('Got new journal entries for notes! Checking ...');
+
+          const promises: List<Promise<any>> = newJournalNoteEntries.map(async (newJournalNoteEntry: Journal) => {
+            console.debug('Requesting note', newJournalNoteEntry.resource_id);
+
+            await this.apiLoad(this.notesService, Note, { id: newJournalNoteEntry.resource_id }, null, async (collectionService: ICollectionService, existingEntries: List<Note>, newEntries: List<Note>) => {
+              return this.syncNoteMerger(collectionService, existingEntries, newEntries);
+            }, null);
+
+            setSyncId(newJournalNoteEntry.id);
+          });
+
+          await Promise.all(promises);
+          console.debug('Updated all notes!');
+        }
+      }
+  }
+
+  private async syncNoteMerger(collectionService: ICollectionService, existingEntries: List<Note>, newEntries: List<Note>) {
+
+  }
+
+  private subscribeToCollectionService(collectionService: ICollectionService, entryType, perEntryCallback: Function|null = null, reflectCallback: Function|null = null): boolean {
     console.debug('Subscribing to collection', collectionService.collectionName, '...');
     const collectionSubscription: Subscription = collectionService.entriesPersisted.subscribe(async (entries: List<any>): Promise<boolean> => {
-      const collectionName: string = collectionService.collectionName;
-      const db: any = collectionService.db;
-
-      if(entries.size === 0) {
-        console.debug('Nothing to persist to local database.');
-        return true;
-      }
-
-      // TODO: Run through db, compare with entries and see, which db entries were deleted in entires, hence require deletion.
-      try {
-        console.debug('Persisting changes to local database ...');
-        const entriesObjects: Array<TEntry> = entries.toJS();
-        console.debug('Got original data:');
-        console.debug(entriesObjects);
-        const rows: Array<Object> = this.rowsFromEntries(entriesObjects);
-        console.debug('Transformed original data:');
-        console.debug(rows);
-        const dbret = await db.bulkDocs(rows);
-        console.debug(dbret);
-
-        const newEntries: List<any> = entries.map((entry: any) => {
-          const entryId = entry.id;
-          const dbretEntry = dbret.find((retEntry) => (retEntry.ok === true && retEntry.id === entryId));
-          return entry.set('_rev', dbretEntry.rev);
-        });
-
-        collectionService.bulkChange(newEntries);
-      } catch(err) {
-        console.error('Could not persist changes to local database:');
-        console.error(err);
-      }
-
-      return true;
+      return this.collectionServiceProcessEntries(collectionService, entryType, entries, perEntryCallback, reflectCallback);
     });
 
     this.collectionSubscriptions.push(collectionSubscription);
+    return true;
+  }
+
+  private async collectionServiceProcessEntries(collectionService: ICollectionService, entryType, entries: List<any>, entriesMerger: Function|null = null, perEntryCallback: Function|null = null, reflectCallback: Function|null = null): Promise<boolean> {
+    const collectionName: string = collectionService.collectionName;
+    const db: any = collectionService.db;
+
+    if(entries.size === 0) {
+      console.debug('Nothing to persist to local database.');
+      return true;
+    }
+
+    const existingEntries: List<any> = await this.collectionServiceLoadExistingEntries(collectionService, entryType);
+    let mergedEntries: List<any> = List();
+
+    console.debug('Merging new values:', entries);
+    console.debug('Merging with existing values:', existingEntries);
+    if(entriesMerger === null) {
+      console.debug('Using regular merger ...');
+      mergedEntries = existingEntries.concat(entries);
+    } else {
+      console.debug('Using custom merger ...');
+      mergedEntries = await entriesMerger(collectionService, existingEntries, entries);
+    }
+
+    try {
+      console.debug('Persisting changes to local database ...', mergedEntries);
+      const mergedEntriesJs: Array<TEntry> = mergedEntries.toJS();
+      console.debug('Got original data:');
+      console.debug(mergedEntriesJs);
+      const mergedEntriesRows: Array<Object> = this.rowsFromEntries(mergedEntriesJs);
+      console.debug('Transformed original data:');
+      console.debug(mergedEntriesRows);
+      // const dbret = await db.bulkDocs(mergedEntriesRows);
+      // console.debug(dbret);
+
+      const newEntriesPromises: List<Promise<any>> = mergedEntries.map(async (entry: any) => {
+        const entryId = entry.id;
+        // const dbretEntry = dbret.find((retEntry) => (retEntry.ok === true && retEntry.id === entryId));
+        let dbretEntry;
+        const newEntry: any = entry.set('_rev', get(dbretEntry, 'rev', undefined));
+
+        if(perEntryCallback !== null) {
+          console.debug('Calling per entry callback ...');
+          return perEntryCallback(collectionService, newEntry);
+        }
+
+        return newEntry;
+      });
+
+      const newEntriesArray: Array<any> = await Promise.all(newEntriesPromises.values());
+      const newEntries: List<any> = List(newEntriesArray);
+
+      if(reflectCallback === null) {
+        console.debug('Using regular reflect callback ...');
+        await this.collectionServiceReflectChanges(collectionService, newEntries);
+      } else {
+        console.debug('Using custom reflect callback ...');
+        await reflectCallback(collectionService, newEntries);
+      }
+    } catch(err) {
+      console.error('Could not persist changes to local database:');
+      console.error(err);
+    }
+
+    return true;
+  }
+
+  private async collectionServiceReflectChanges(collectionService: ICollectionService, entries: List<any>): Promise<boolean> {
+    console.debug('Propagating:', entries);
+    await collectionService.bulkChange(entries);
     return true;
   }
 
@@ -97,6 +229,25 @@ export class SyncService {
     return true;
   }
 
+  async collectionServiceLoadExistingEntries(collectionService: ICollectionService, entryType): Promise<List<TEntry>> {
+    console.debug('collectionServiceLoadExistingEntries ...');
+    const allDocs: TPouchResponse = await collectionService.collection.allDocs({include_docs: true});
+
+    console.debug('All existing documents:', allDocs);
+
+    const entries: Array<TEntry> = map(allDocs.rows, (row: TPouchResponseRow) => {
+      const entry = omit(row.doc, ['_id']);
+      entry.id = row.doc._id;
+      console.debug('Loading the following row:');
+      console.debug(entry);
+      return new entryType(entry);
+    });
+
+    console.debug('Converted existing documents:', entries);
+
+    return List(entries);
+  }
+
   private rowFromEntry(entry: TEntry): Object {
     const row = omit(entry, ['id']);
     row._id = entry.id;
@@ -110,12 +261,12 @@ export class SyncService {
     });
   }
 
-  private async apiLoad(collectionService: ICollectionService) {
-    await collectionService.apiList()
+  private async apiLoad(collectionService: ICollectionService, entryType, params = {}, entriesMerger: Function|null = null, perEntryCallback: Function|null = null, reflectCallback: Function|null = null) {
+    return collectionService.apiList(params)
       .subscribe(
         (entries: List<Record<TEntry>>) => {
           if(entries.size > 0) {
-            collectionService.bulkChange(entries);
+            this.collectionServiceProcessEntries(collectionService, entryType, entries, entriesMerger, perEntryCallback, reflectCallback);
           }
         },
         (error) => {
